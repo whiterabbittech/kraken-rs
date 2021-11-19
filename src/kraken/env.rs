72548cc34a29f7f8cbb4e2fe0f3f22f14b890593
Robbie McKinstry <thesnowmancometh@gmail.com>
2021-11-19 11:52:13 -0500

    Add Credentials mechanism (#15)

---
+++ src/kraken/env.rs
@@ -0,0 +1,27 @@
+const KRAKEN_API_KEY: &str = "KRAKEN_API_KEY";
+const KRAKEN_PRIVATE_KEY: &str = "KRAKEN_PRIVATE_KEY";
+
+pub struct KrakenCredentials {
+    api_key: String,
+    private_key: String,
+}
+
+impl KrakenCredentials {
+    pub fn new_from_env() -> Result<Self, std::env::VarError> {
+        let api_key = std::env::var(KRAKEN_API_KEY)?;
+        let private_key = std::env::var(KRAKEN_PRIVATE_KEY)?;
+        Ok(Self::new(api_key, private_key))
+    }
+
+    pub fn new(api_key: String, private_key: String) -> Self {
+        Self {api_key, private_key}
+    }
+
+    pub fn api_key(&self) -> &String {
+        &self.api_key
+    }
+
+    pub fn private_key(&self) -> &String {
+        &self.private_key
+    }
+}
